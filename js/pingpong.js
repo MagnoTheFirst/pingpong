c7d7ef10ceda2da5e3b4a266bdb1ade59188d1bd
alejandro.laneri@students.ffhs.ch <alejandro.laneri@students.ffhs.ch>
2021-08-13 13:51:24 +0200

    animation does not work

--- js/pingpong.js
+++ js/pingpong.js
@@ -1,5 +1,6 @@
 (function($) {
     $(function() {
+        init();
         // alert a message     
         alert("Welcome to the Ping Pong battle.");
         var pingpong = {
@@ -15,12 +16,35 @@
                 width: 20,
                 heigth: 70
             },
+            playground: {
+                offsetTop: $("#playground").offsetTop(),
+            }
         };
+
+        function handleMouseInputs() {
+            // run the game when mouse moves in the playground.   
+            $('#playground').mouseenter(function() {
+                pingpong.isPaused = false;
+            });
+            // pause the game when mouse moves out the playground.   
+            $('#playground').mouseleave(function() {
+                pingpong.isPaused = true;
+            });
+            // calculate the paddle position by using the mouse position.
+            $('#playground').mousemove(function(e) {
+                pingpong.paddleB.y = e.pageY - pingpong.playground.offsetTop;
+            });
+        }
         // view rendering   
         function renderPaddles() {
-            $("#paddleB").css("top", pingpong.paddleB.y);
-            $("#paddleA").css("top", pingpong.paddleA.y);
+            renderPaddles();
+            window.requestAnimationFrame(render);
         }
-        renderPaddles();
+
+        function init() { // view rendering   
+            window.requestAnimationFrame(render);
+            // inputs   
+            handleMouseInputs();
+        }
     });
 })(jQuery);
