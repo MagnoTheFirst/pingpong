6e85058a3e0d786fbbdf3f9efd9e02832639a68d
alejandro.laneri@students.ffhs.ch <alejandro.laneri@students.ffhs.ch>
2021-08-13 13:21:41 +0200

    implemnented logic for game

--- js/pingpong.js
+++ js/pingpong.js
@@ -2,5 +2,25 @@
     $(function() {
         // alert a message     
         alert("Welcome to the Ping Pong battle.");
+        var pingpong = {
+            paddleA: {
+                x: 50,
+                y: 100,
+                width: 20,
+                heigth: 70
+            },
+            paddleB: {
+                x: 320,
+                y: 100,
+                width: 20,
+                heigth: 70
+            },
+        };
+        // view rendering   
+        function renderPaddles() {
+            $("#paddleB").css("top", pingpong.paddleB.y);
+            $("#paddleA").css("top", pingpong.paddleA.y);
+        }
+        renderPaddles();
     });
 })(jQuery);
